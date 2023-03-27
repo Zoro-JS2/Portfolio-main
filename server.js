e38274503dbbc205efe3396ccc6e93ed17890f98
Rigidbody-JS <66162438+Zoro-JS2@users.noreply.github.com>
2023-03-27 21:13:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,15 @@
 // Run app: npm run
 const express = require("express");
 const app = express();
+require('dotenv').config();
 const mongoose = require("mongoose");
-const PORT = 3000;
 const { MongoClient } = require("mongodb");
 const { cache } = require("ejs");
 const axios = require("axios");
 const libraries = require("./server/db.json");
 
-const admin = {
-  name: "Dmytro",
-  password: "d3e4m5a2",
-  foldername: "nodeDB2",
-};
-const client = new MongoClient(
-  `mongodb+srv://${admin.name}:${admin.password}@cluster0.rvkcx.mongodb.net/${admin.foldername}?retryWrites=true&w=majority`
-);
+
+const client = new MongoClient(process.env.MONGO_URL);
 /*  //connect to Mongo DB
 const db="mongodb+srv://Dmytro:d3e4m5a2@cluster0.rvkcx.mongodb.net/nodeDB?retryWrites=true&w=majority"; 
 
@@ -24,7 +18,7 @@ const db="mongodb+srv://Dmytro:d3e4m5a2@cluster0.rvkcx.mongodb.net/nodeDB?retryW
 mongoose.connect(db).then((res)=>console.log("Connected to DB"));
  */
 //const __dirname=path.resolve();
-app.listen(3000, () => console.log(`Server Started. PORT: ${PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server Started. PORT: ${process.env.PORT}`));
 
 app.get("/", (req, res) => {
   res.send("Hello. Server is Good!");
